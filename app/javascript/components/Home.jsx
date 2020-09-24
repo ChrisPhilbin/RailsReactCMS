c18a225c473caacb43562f07f24c372971c443e9
Chris Philbin <chris@chrisphilbin.net>
2020-09-24 07:11:05 -0400

    Adding conditional logic to home in order to render based on redux state changing

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,55 +1,4 @@
-// import React, {useState, useEffect} from 'react'
-// import { Link } from 'react-router-dom'
-
-//   const App = () => {
-
-//     const [latestPosts, setLatestPosts] = useState([])
-
-//     useEffect(() => {
-//       fetch('/api/v1/posts/latest')
-//       .then(response => {
-//         if (response.ok) {
-//           return response.json()
-//         }
-//         throw new Error("Network response not ok")
-//       })
-//       .then(response => setLatestPosts(latestPosts.concat(response)))
-//     }, [])
-
-//     const allPosts = latestPosts.map((post, index) => (
-//       <div key={index} className="col-md-6 col-lg-4">
-//           <div className="card mb-4">
-//               <div className="card-body">
-//                   <h5 className="card-title">
-//                       <Link to={'/posts/' + post.id}>{post.title}</Link>
-//                   </h5>
-//               </div>
-//           </div>
-//       </div>
-//   ))
-//   const noPosts = (
-//       <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
-//           <h4>
-//               No posts yet.  Why not create one?
-//           </h4>
-//       </div>
-//   )
-
-//     return(
-//       <div>
-//         <div className="py-5">
-//             <div className="row">
-//                 {latestPosts.length > 0 ? allPosts : noPosts}
-//             </div>
-//         </div>
-//       </div>
-//     )
-
-//   }
-
-// export default App
-
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { fetchLatestPosts } from './actions/PostsActions'
@@ -66,17 +15,37 @@ const App = () => {
     dispatch(fetchLatestPosts())
   }, [])
 
+  let status
   let showPosts
 
   if (loading) {
-    showPosts = (
+    status = (
       <div><h1>Loading posts...</h1></div>
     )
-  } else {
-    showPosts = (
-      <div>Showing the most recent posts</div>
-    )
   }
+
+  if (allPosts.length > 0) {
+    showPosts = ( allPosts.map((post, index) => (
+      <div key={index} className="col-md-6 col-lg-4">
+          <div className="card mb-4">
+              <div className="card-body">
+                  <h5 className="card-title">
+                      <Link to={'/posts/' + post.id}>{post.title}</Link>
+                  </h5>
+              </div>
+          </div>
+      </div>
+    )))
+  }
+
+  return(
+    <div className="py-5">
+      <div className="row">
+        {status}
+        {showPosts}
+      </div>
+    </div>
+  )
 }
 
 export default App
