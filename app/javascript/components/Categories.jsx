91d819f4236740af0757810ca3952d1a89e9d088
Chris Philbin <chris@chrisphilbin.net>
2020-10-15 07:10:05 -0400

    Implementing Redux on the Categories page

--- app/javascript/components/Categories.jsx
+++ app/javascript/components/Categories.jsx
@@ -1,50 +1,82 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
+// import React, {useEffect, useState} from 'react'
+// import {Link} from 'react-router-dom'
+
+// const Categories = () => {
+
+//     const [categories, setCategories] = useState([])
+
+//     useEffect(() => {
+//         const url = '/api/v1/categories'
+//         fetch(url)
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.json()
+//                 }
+//                 throw new Error("Network response not ok")
+//             })
+//             .then(response => setCategories(categories.concat(response)))
+//     }, [])
+
+//     const allCategories = categories.map((category, index) => (
+//         <div key={index} className="col-md-6 col-lg-4">
+//             <div className="card mb-4">
+//                 <div className="card-body">
+//                     <h5 className="card-title">
+//                         <Link to={'/categories/' + category.id}>{category.name}</Link>
+//                     </h5>
+//                 </div>
+//             </div>
+//         </div>
+//     ))
+
+//     const noCategories = (
+//         <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+//             <h4>
+//                 No Categories yet.  Why not create one?
+//             </h4>
+//         </div>
+//     )
+
+//     return(
+//         <div>
+//             <div className="py-5">
+//                 <div className="row">
+//                     {categories.length > 0 ? allCategories : noCategories}
+//                 </div>
+//             </div>
+//         </div>
+
+//     )
+
+// }
+
+// export default Categories
+
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCategories } from './actions/CategoriesActions'
+import { loadingMessage } from './actions/SessionActions'
 
 const Categories = () => {
 
-    const [categories, setCategories] = useState([])
+    const dispatch = useDispatch()
+    const categories = useSelector(state => state.categories.allCategories)
+    const loading    = useSelector(state => state.categories.loading)
+    const hasErrors  = useSelector(state => state.categories.hasErrors)
 
-    useEffect(() => {
-        const url = '/api/v1/categories'
-        fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
-                }
-                throw new Error("Network response not ok")
-            })
-            .then(response => setCategories(categories.concat(response)))
+    let allCategories, noCategories
+
+    useEffect( () => {
+        dispatch(fetchCategories())
     }, [])
 
-    const allCategories = categories.map((category, index) => (
-        <div key={index} className="col-md-6 col-lg-4">
-            <div className="card mb-4">
-                <div className="card-body">
-                    <h5 className="card-title">
-                        <Link to={'/categories/' + category.id}>{category.name}</Link>
-                    </h5>
-                </div>
-            </div>
-        </div>
-    ))
+    if (categories) {
+        allCategories = (
 
-    const noCategories = (
-        <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
-            <h4>
-                No Categories yet.  Why not create one?
-            </h4>
-        </div>
-    )
+        )
+    }
 
     return(
-        <div>
-            <div className="py-5">
-                <div className="row">
-                    {categories.length > 0 ? allCategories : noCategories}
-                </div>
-            </div>
-        </div>
 
     )
 
--- app/javascript/components/Posts.js
+++ app/javascript/components/Posts.js
@@ -1,68 +1,3 @@
-// import React, {useEffect, useState} from 'react'
-// import { Link } from 'react-router-dom'
-
-// const Posts = (props) => {
-
-//     const [posts, setPosts] = useState([])
-
-//     let user_id = props.user_id
-
-//     useEffect(() => {
-//         const url = '/api/v1/posts/index'
-//         fetch(url)
-//             .then(response => {
-//                 if (response.ok) {
-//                     return response.json()
-//                 }
-//                 throw new Error("Network response not ok")
-//             })
-//             .then(response => setPosts(posts.concat(response)))
-//             .then(console.log(posts, "POSTS"))
-//             .catch(() => this.props.history.push('/'))
-//     }, [])
-
-//     const allPosts = posts.map((post, index) => (
-//         <div key={index} className="col-md-6 col-lg-4">
-//             <div className="card mb-4">
-//                 <div className="card-body">
-//                     <h5 className="card-title">
-//                         <Link to={'/posts/' + post.id}>{post.title}</Link>
-//                     </h5>
-//                 </div>
-//             </div>
-//         </div>
-//     ))
-//     const noPosts = (
-//         <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
-//             <h4>
-//                 No posts yet.  Why not create one?
-//             </h4>
-//         </div>
-//     )
-
-//     const newPostButton = (
-//         <Link to={'/posts/new'} className="btn btn-primary" role="button">New post</Link>
-//     )
-
-//     const signInButton = (
-//         <a href="/users/sign_in"><button className="btn btn-primary">Sign in</button></a>
-//     )
-
-//     return(
-//             <>
-//                 {user_id != "not_signed_in" ? newPostButton : signInButton}
-
-//                 <div className="py-5">
-//                     <div className="row">
-//                         {posts.length > 0 ? allPosts : noPosts}
-//                     </div>
-//                 </div>
-//             </>
-//     )
-// }
-
-// export default Posts
-
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -79,12 +14,6 @@ const Posts = () => {
     useEffect(() => {
         dispatch(fetchAllPosts())
     }, [])
-
-    useEffect(() => {
-        return () => {
-            posts = []
-        }
-    })
 
     let showPosts
     
