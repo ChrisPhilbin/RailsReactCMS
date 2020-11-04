753ad2fd53f0b4c57396659e0ce8c86c59932161
Chris Philbin <chris@chrisphilbin.net>
2020-11-04 06:19:43 -0500

    Abstrating away categories from the sidebar and making the sidebar component reuseable

--- app/javascript/components/navigation/SideBar.jsx
+++ app/javascript/components/navigation/SideBar.jsx
@@ -1,43 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { fetchCategories } from '../actions/CategoriesActions'
-import { Loading } from 'images/loading.gif'
+import React from 'react'
+import CategoriesWidget from './widgets/CategoriesWidget'
 
 const SideBar = () => {
 
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(fetchCategories())
-    }, [])
-
-    const categories = useSelector(state => state.categories.allCategories)
-    const loading    = useSelector(state => state.categories.loading)
-    const hasErrors  = useSelector(state => state.categories.hasErrors)
-
-    let showCategories, showLoading
-
-    if (loading) {
-        showLoading = (
-            <img src={Loading} />
-        )
-    }
-
-    if (categories.length > 0) {
-        showCategories = (
-            categories.map((category, index) => (
-                <div key={index}>
-                    <Link to={'/categories/' + category.id}>{category.name}</Link>
-                </div>
-            ))
-        )
-    }
-
     return(
-        <div className="categories-column">
-            {showLoading}
-            {showCategories}
+        <div className="sidebar-column">
+            {<CategoriesWidget />}
         </div>
     )
 }
